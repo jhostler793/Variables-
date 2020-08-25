4ece5d514d648c4c015198e69722fec5c8bccc34
jhostler793 <jmh12@hood.edu>
2020-08-25 03:03:22 -0400

    Variables

---
+++ src/Variables.java
@@ -0,0 +1,13 @@
+public class Variables {
+    public static void main(String[] args) {
+
+        int testScore=50;
+        double priceTag = 24.99;
+        String userName="Bobby Johnson";
+
+       System.out.println("Test score initialized is: " + testScore);
+       System.out.println("Price tag: "+ priceTag);
+       System.out.println("Username: " + userName);
+
+    }
+}
